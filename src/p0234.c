783187ffcbdf17b5f86a40a4e73f6b97e8227a41
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2020-01-03 13:41:39 -0600

    Problem 234: improve the solution * Break logic into more atomic functions * Use const where needed

--- src/p0234.c
+++ src/p0234.c
@@ -13,65 +13,97 @@ struct ListNode {
 };
 
 
-bool is_palindrom(struct ListNode* head)
+const struct ListNode * get_second_half_pointer(const struct ListNode *head)
 {
-    struct ListNode *slow, *fast, *second_half = NULL, *temp;
-    bool palindrome = true;
-    /* Cover edge cases */
+    /*
+     * Returns a pointer to the first element in the 2nd half of a linked list.
+     * If the linked list has odd number of nodes, pointer to element after mid
+     * element is returned.
+     */
+    const struct ListNode *fast = head;
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        head = head->next;
+    }
+    /* If odd number of nodes, skip the mid node */
+    if (fast != NULL)
+        head = head->next;
+    return head;
+}
+
+
+bool linked_list_starts_with(
+    const struct ListNode *main_list,
+    const struct ListNode *partial_list
+)
+{
+    /*
+     * Returns true if `main_list` starts with `partial_list`.
+     */
+    bool result = true;
+    while (
+        partial_list != NULL
+        && main_list != NULL
+        && partial_list->val == main_list->val
+    ) {
+        main_list = main_list->next;
+        partial_list = partial_list->next;
+    }
+    if (partial_list != NULL)
+        result = false;
+    return result;
+}
+
+
+void free_linked_list(struct ListNode *head)
+{
+    /*
+     * Frees up dynamic memory allocated to linked list.
+     */
+    struct ListNode *temp;
+    while (head != NULL) {
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+}
+
+
+struct ListNode * create_reversed_linked_list(const struct ListNode *head)
+{
+    /*
+     * Creates a new reversed linked list (without changing the original list)
+     */
+    struct ListNode *temp, *reversed_list = NULL;
+    while (head != NULL) {
+        temp = (struct ListNode *) malloc(sizeof(struct ListNode));
+        temp->val = head->val;
+        temp->next = reversed_list;
+        reversed_list = temp;
+        head = head->next;
+    }
+    return reversed_list;
+}
+
+
+bool is_palindrom(struct ListNode *head)
+{
+    /*
+     * Checks if the linkeds list is a palindrome.
+     */
+    const struct ListNode *second_half;
+    struct ListNode *second_half_reversed;
+    bool result;
     if (head == NULL)
         return false;
     if (head->next == NULL)
         return true;
-    /* Find middle node */
-    slow = head;
-    fast = head;
-    while (fast != NULL && fast->next != NULL) {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    if (fast != NULL)
-        slow = slow->next;
+    second_half = get_second_half_pointer(head);
     /* Create a reversed linked list for the 2nd half */
-    while (slow != NULL) {
-        temp = (struct ListNode *) malloc(sizeof(struct ListNode));
-        temp->val = slow->val;
-        temp->next = second_half;
-        second_half = temp;
-        slow = slow->next;
-    }
-    /* Compare 1st half and reversed 2nd half */
-    temp = second_half;
-    while (second_half != NULL) {
-        if (head->val != second_half->val) {
-            palindrome = false;
-            break;
-        }
-        second_half = second_half->next;
-        head = head->next;
-    }
-    /* Free allocated memory */
-    second_half = temp;
-    while (second_half != NULL) {
-        temp = second_half->next;
-        free(second_half);
-        second_half = temp;
-    }
-    return palindrome;
-}
-
-
-int main()
-{
-    struct ListNode a, b, c;
-    a.val = 1;
-    b.val = 2;
-    c.val = 1;
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-    if (is_palindrom(&a))
-        printf("Yes\n");
-    else
-        printf("No\n");
-    return 0;
+    second_half_reversed = create_reversed_linked_list(second_half);
+    result = linked_list_starts_with(head, second_half_reversed);
+    free_linked_list(second_half_reversed);
+    return result;
 }
